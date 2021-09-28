21df531fc3d97ca113b7d10f1c8a1221ad43f6d7
Olav Haaland Moseng <olav.moseng@gmail.com>
2021-09-28 19:38:46 +0200

    Updated App.js to use new component Washinglist.js

---
+++ src/components/Washinglist.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Washinglist() {
+
+    return (
+        <div>
+            <table>
+                <tr>
+                    <td>🧽🚽</td>
+                    <td>
+                        <input id="check-box" type="checkbox"/>
+                        <label htmlFor="check-box">Navn</label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    )
+}
